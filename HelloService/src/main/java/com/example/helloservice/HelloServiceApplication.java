93e567f96e211d065ae2cb1d434086ed0f7f3c7c
xielbs <928135874@qq.com>
2018-04-19 10:57:21 +0800

    修改

--- HelloService/src/main/java/com/example/helloservice/HelloServiceApplication.java
+++ HelloService/src/main/java/com/example/helloservice/HelloServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SpringbootApplication {
+public class HelloServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
 
--- HelloService/src/test/java/com/example/helloservice/HelloServiceApplicationTests.java
+++ HelloService/src/test/java/com/example/helloservice/HelloServiceApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringbootApplication.class)
+@SpringApplicationConfiguration(classes = HelloServiceApplication.class)
 @WebAppConfiguration
-public class SpringbootApplicationTests {
+public class HelloServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
