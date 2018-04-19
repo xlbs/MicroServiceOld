278a216df696a6245f0b54467db0b5e06b30d451
xielbs <928135874@qq.com>
2018-04-19 10:51:16 +0800

    修改

--- HelloService/src/main/java/com/example/helloservice/web/HelloController.java
+++ HelloService/src/main/java/com/example/helloservice/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springboot.web;
+package com.example.helloservice.web;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
--- HelloService/src/test/java/com/example/helloservice/SpringbootApplicationTests.java
+++ HelloService/src/test/java/com/example/helloservice/SpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.example.helloservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
