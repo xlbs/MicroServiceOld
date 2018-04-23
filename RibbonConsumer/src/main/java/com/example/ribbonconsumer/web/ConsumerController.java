722f7c9eb346e9c42032ca3b6d8f8692fc4a135d
xielbs <928135874@qq.com>
2018-04-23 18:59:34 +0800

    修改

--- RibbonConsumer/src/main/java/com/example/ribbonconsumer/web/ConsumerController.java
+++ RibbonConsumer/src/main/java/com/example/ribbonconsumer/web/ConsumerController.java
@@ -6,15 +6,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class ConsumerController {
 
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "testhello",method = RequestMethod.GET)
-    public String helloConsumer(){
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
+    @RequestMapping(value = "testhello",method = RequestMethod.POST)
+    public List<Map> helloConsumer(String sql){
+        List<Map> list = new ArrayList<>();
+//        list = restTemplate.postForObject("http://DATAOPERT-SERVICE/querySql?sql={1}",String.class,List.class,sql);
+        try {
+            String strUrl = "http://DATAOPERT-SERVICE/querySql?sql="+sql+"";
+            URL url = new URL(strUrl);
+            URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+            list = restTemplate.postForObject(uri,String.class,List.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(list.toString());
+        return list;
+
+//        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
     }
 
 
