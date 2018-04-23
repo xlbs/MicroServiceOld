bb3dcc579636b2bdad467fead0eb69c11271cfcd
xielbs <928135874@qq.com>
2018-04-23 20:01:11 +0800

    修改

--- DataOperatService/src/main/java/com/xlbs/dataoperatservice/DataoperatserviceApplication.java
+++ DataOperatService/src/main/java/com/xlbs/dataoperatservice/DataoperatserviceApplication.java
@@ -3,8 +3,10 @@ package com.xlbs.dataoperatservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableDiscoveryClient
+@EnableDiscoveryClient//c创建针对Eureka客户端的实例
+@EnableTransactionManagement//开启事务管理
 @SpringBootApplication
 public class DataoperatserviceApplication {
 
