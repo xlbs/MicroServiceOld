1521b390d3eafa1dc073ae62cefc760bcc9a3580
xielbs <928135874@qq.com>
2018-04-25 19:58:13 +0800

    修改

--- RibbonConsumer/src/main/java/com/example/ribbonconsumer/web/ConsumerController.java
+++ RibbonConsumer/src/main/java/com/example/ribbonconsumer/web/ConsumerController.java
@@ -1,14 +1,16 @@
 package com.example.ribbonconsumer.web;
 
+import com.example.ribbonconsumer.commutils.JSONUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,22 +20,37 @@ public class ConsumerController {
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "testhello",method = RequestMethod.POST)
-    public List<Map> helloConsumer(String sql){
-        List<Map> list = new ArrayList<>();
+    @RequestMapping(value = "testhello",method = {RequestMethod.POST,RequestMethod.GET})
+    public void helloConsumer(){
 //        list = restTemplate.postForObject("http://DATAOPERT-SERVICE/querySql?sql={1}",String.class,List.class,sql);
+//        restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
         try {
-            String strUrl = "http://DATAOPERT-SERVICE/querySql?sql="+sql+"";
-            URL url = new URL(strUrl);
-            URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
-            list = restTemplate.postForObject(uri,String.class,List.class);
+//            URL url = new URL(strurl);
+//            URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+//            String strurl = "http://DATAOPERT-SERVICE/querySql";
+//            String sql = "select * from s_user";
+//            MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+//            map.add("sql",sql);
+//            List<Map> list = restTemplate.postForObject(strurl,map,List.class);
+//            System.out.println(list.toString());
+
+//            URL url = new URL(strurl);
+//            URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+            List<String> list = new ArrayList<>();
+            String sql ="";
+            sql = "INSERT INTO s_user (USER_NO,USER_NAME,ORG_ID,USER_PWD,IS_VALID) VALUES ('6666','fff',200,'ggg','F_YES')";
+            list.add(sql);
+            sql = "";
+            sql = "INSERT INTO s_user (USER_NO,USER_NAME,ORG_ID,USER_PWD,IS_VALID) VALUES ('777','ddd',200,'ggg','F_YES')";
+            list.add(sql);
+            String sqlList = JSONUtils.serialize(list);
+            String strurl = "http://DATAOPERT-SERVICE/executeTrans";
+            String result = restTemplate.postForObject(strurl, sqlList, String.class);
+            System.out.println(result);
         }catch (Exception e){
             e.printStackTrace();
         }
-        System.out.println(list.toString());
-        return list;
 
-//        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
     }
 
 
